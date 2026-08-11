6d0dc2faae82e568f1b982fe37150f576752f15d
holyrocking <zggshsgsgsh@gmail.com>
2026-08-11 23:17:42 +0300

    feat: add edit profile zustand slice

---
+++ src/features/profile/types/store/editUserSlice.ts
@@ -0,0 +1,133 @@
+import type { StateCreator } from "zustand";
+import type { ProfileLink } from "../side-cards/profilelinks";
+import type { UserProfile } from "../user/userProfile"
+
+
+
+type EditUserState = {
+    draft:UserProfile | null;
+}
+
+type EditUserActions = {
+
+    initDraft:()=>void;
+
+    updateField: (field: keyof UserProfile, value: string) => void;
+    setAvatar:(avatar:string)=>void;
+    setBanner:(banner:string)=>void;
+    addLink:(link:ProfileLink)=>void;
+
+    updateLink:(type:ProfileLink["type"],
+        link:string
+    )=>void;
+
+    removeLink:(
+        type:ProfileLink["type"]
+    )=>void;
+
+    resetDraft:()=>void;
+    clearDraft:()=>void;
+
+}
+
+export type EditUserSlice = EditUserState & EditUserActions;
+
+const initialState: EditUserState = {
+  draft: null,
+};
+
+export const createEditUserSlice: StateCreator<
+  EditUserSlice,
+  [],
+  [],
+  EditUserSlice
+> = (set,get) => ({
+  ...initialState,
+
+  initDraft: (user) =>
+    set(() => ({
+    draft: { ...get().user },
+  })),
+
+  updateField: (field, value) =>
+    set((state) => ({
+      draft: state.draft
+        ? {
+            ...state.draft,
+            [field]: value,
+          }
+        : null,
+    })),
+    setAvatar:(avatar)=>
+        set((state)=>({
+            draft:state.draft
+            ?{
+                ...state.draft,
+                avatar,
+            }
+            : null,
+        })),
+
+    setBanner:(banner)=>
+        set((state)=>({
+            draft:state.draft
+            ?
+            {
+                ...state.draft,
+                banner
+            }
+            : null,
+            }
+            
+        )),
+
+        addLink:( link)=>
+            set((state)=>({
+                draft:state.draft
+                ?
+                {
+                    ...state.draft,
+                    links: [...state.draft.links, link],
+                }
+                :null,
+            })),
+
+            updateLink: (type, link) =>
+                 set((state) => ({
+                draft: state.draft
+                ? {
+                    ...state.draft,
+                    links: state.draft.links.map((item) =>
+                        item.type === type
+                        ? { ...item, link }
+                        : item
+                    ),
+                    }
+                : null,
+            })),
+
+            removeLink:(type)=>
+                set((state)=>({
+                    draft:state.draft
+                    ?
+                    {
+                    ...state.draft,
+                    links:state.draft.links.filter(item=> item.type!== type)
+                    }
+                    : null
+                })),
+
+            resetDraft:()=>
+                 set(() => ({
+            draft: { ...get().user },
+        })),
+
+          
+
+        clearDraft:()=>
+            set(()=>({
+                draft:null
+            }))
+
+
+});
